e0369d36980b48d9a248a8eb94e373e4741157f2
Dave Brown <dave.brown@exit107.com>
2022-03-26 11:03:01 -0600

    Add Divide function and test

--- calculator_test.go
+++ calculator_test.go
@@ -75,3 +75,33 @@ func TestMultiply(t *testing.T) {
 		}
 	}
 }
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		name        string
+		a, b        float64
+		want        float64
+		errExpected bool
+	}
+
+	testCases := []testCase{
+		{name: "test case name", a: 2, b: 2, want: 1, errExpected: false},
+		{name: "test case name", a: 1, b: 5, want: .2, errExpected: false},
+		{name: "test case name", a: 5, b: 0, want: 0, errExpected: true},
+		{name: "test case name", a: -5, b: 1, want: -5, errExpected: false},
+	}
+
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err != nil && !tc.errExpected {
+			t.Errorf("Multiply(%f, %f): Unexpected error returned.", tc.a, tc.b)
+		}
+		if err == nil && tc.errExpected {
+			t.Errorf("Multiply(%f, %f): Expected error not found.", tc.a, tc.b)
+		}
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
